5e673eb52d77405730fa7e84286cb048bb6e76b1
FernandoTarso <fernando.tarso.git@gmail.com>
2024-04-06 22:39:41 -0300

    js add, projetos tilulo fix

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const botaoMaisProjetos = document.querySelector(".botao-mais");
+const botaoEsconder = document.querySelector(".botao-esconder");
+const projetosInativos = document.querySelectorAll(".hide");
+const projetosAtivos = document.querySelectorAll(".show");
+console.log(botaoMaisProjetos);
+botaoMaisProjetos.addEventListener("click", () => {
+  mostrarMaisProjetos();
+  esconderBotao();
+});
+function esconderBotao() {
+  botaoMaisProjetos.classList.add("hide");
+}
+function mostrarMaisProjetos() {
+  projetosInativos.forEach((projetoInativo) => {
+    projetoInativo.classList.add("show");
+  });
+}
+botaoEsconder.addEventListener("click", () => {
+  esconderMaisProjetos();
+  mostrarBotao();
+});
+function mostrarBotao() {
+  botaoMaisProjetos.classList.remove("hide");
+}
+function esconderMaisProjetos() {
+  projetosInativos.forEach((projetoInativo) => {
+    projetoInativo.classList.remove("show");
+  });
+}
